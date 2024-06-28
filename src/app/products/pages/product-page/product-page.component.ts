9fc6fb8ea952131966dc344494934c57fb830e17
Juanse Franco <jsfc2199@gmail.com>
2024-06-28 11:42:07 -0500

    Creacion de formulario reactivo tradicional

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ProductPageComponent {
 
+  // constructor(private fb: FormBuilder) {}
+
+  //otro tipo de inyección de dependencias enfocado a signals
+  private fb = inject(FormBuilder)
+
+  color: string = 'green'
+
+  myForm: FormGroup = this.fb.group({
+    name:['', [Validators.required, Validators.minLength(6), Validators.email]]
+  })
+
+  changeColor(){
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    this.color = color
+  }
+
+  //TODO crear directiva personalizada para mostrar los errores sin tener que hacer tanta 'carpintería'
 }
